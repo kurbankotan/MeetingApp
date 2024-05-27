b5ee920aecb149fb4fdb4d71c8932801d51a8bc6
Kurban KOTAN <kurbankotan@gmail.com>
2024-05-27 12:48:32 +0300

    Adding Home&Meeting Cotrollers

---
+++ Controllers/MeetingController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    
+    public class MeetingController : Controller
+    {
+
+        public string Index()
+        {
+            return "meeting/index";
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/abc", () => "Deneme");
+//Böyle tek tek tanımlanabilir
+// app.MapGet("/", () => "Hello World!");
+// app.MapGet("/abc", () => "Deneme");
+
+
+// Ya da genel olarak tanımlanabilir
+//{controller=Home}/{action=Index}/{id?}
+//app.MapDefaultControllerRoute();
+
+//Ya da genel yazımın daha uzun şekli olarak aşağıdaki gibi yazılabilir
+app.MapControllerRoute(
+    name:"default",
+    pattern: "{controller=Meeting}/{action=Index}/{id?}"
+);
+
+
+
 
 app.Run();
